5b64c07bd90d27ace1a72baabdc062285799d394
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2023-01-06 20:37:57 -0800

    Added express

--- Backend/index.ts
+++ Backend/index.ts
@@ -43,3 +43,17 @@ async function run() {
   
     console.log(user.first);
 }
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get( "/", ( req, res ) => {
+    res.send('Hello World!')
+} );
+
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
